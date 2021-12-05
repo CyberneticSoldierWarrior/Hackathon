e96a661e5b2e935e86032cc1373aaeacd1dec2dc
Jesus Genes <jesus.genes.mt@correo.usa.edu.co>
2021-12-05 18:43:36 -0500

    Terminos y Condiciones parte 1

---
+++ app/src/main/java/usa/salamanca/zonas/Vista/Condiciones.java
@@ -0,0 +1,14 @@
+package usa.salamanca.zonas.Vista;
+
+import androidx.appcompat.app.AppCompatActivity;
+import usa.salamanca.zonas.R;
+import android.os.Bundle;
+
+public class Condiciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_condiciones);
+    }
+}
--- app/src/main/java/usa/salamanca/zonas/Vista/RegistroActivity.java
+++ app/src/main/java/usa/salamanca/zonas/Vista/RegistroActivity.java
@@ -2,7 +2,11 @@ package usa.salamanca.zonas.Vista;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import usa.salamanca.zonas.R;
 
@@ -12,5 +16,15 @@ public class RegistroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        Button botonCondiciones = (Button) findViewById(R.id.btn_condiciones);
+        botonCondiciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Aceptar Terminos", Toast.LENGTH_SHORT).show();
+                Intent CondicionesActivity = new Intent(getApplicationContext(), Condiciones.class);
+                startActivity(CondicionesActivity);
+            }
+        });
     }
 }
