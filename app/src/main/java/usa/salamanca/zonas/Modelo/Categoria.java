9c1de3b8ad1563edae76464ea79c8ef3ce3015cd
Jesus Genes <jesus.genes.mt@correo.usa.edu.co>
2021-12-06 06:35:26 -0500

    Creacion de la clase User parte 2

--- app/src/main/java/usa/salamanca/zonas/Modelo/Categoria.java
+++ app/src/main/java/usa/salamanca/zonas/Modelo/Categoria.java
@@ -1,2 +1,15 @@
-package usa.salamanca.zonas.Modelo;public class Categoria {
+package usa.salamanca.zonas.Modelo;
+
+public class Categoria {
+
+    private String nombrecat;
+
+    public Categoria(){
+        this.nombrecat = "";
+    }
+
+    public String getNombrecat(){ return this.nombrecat; }
+
+    public void setNombrecat(){ this.nombrecat = nombrecat; }
+
 }
--- app/src/main/java/usa/salamanca/zonas/Modelo/User.java
+++ app/src/main/java/usa/salamanca/zonas/Modelo/User.java
@@ -1,4 +1,50 @@
 package usa.salamanca.zonas.Modelo;
 
-public class User {
+public class User {;
+
+    private String firstname;
+
+    private String lastname;
+
+    private Integer age;
+
+    private Integer movetype;
+
+    private String email;
+
+    private String clave;
+
+    public User(){
+        this.firstname = "";
+        this.lastname = "";
+        this.age = 0;
+        this.movetype = 0;
+        this.email = "";
+        this.clave = "";
+    }
+
+    public String getFirstname(){ return this.firstname; }
+
+    public void setFirstname(String firstname){ this.firstname = firstname; }
+
+    public String getLastname(){ return this.lastname; }
+
+    public void setLastname(String lastname){ this.lastname = lastname; }
+
+    public Integer getAge(){ return this.age; }
+
+    public void setAge(Integer age){ this.age = age; }
+
+    public Integer getMovetype(){ return this.movetype; }
+
+    public void setMovetype(Integer movetype){ this.movetype = movetype; }
+
+    public String getEmail(){ return this.email; }
+
+    public void setEmail(String email){ this.email = email; }
+
+    public String getClave(){ return this.clave; }
+
+    public void setClave(String clave) { this.clave = clave; }
+
 }
